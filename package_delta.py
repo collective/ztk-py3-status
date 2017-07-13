02fd34ece265f38bdcb9211866e6d2f0b801c811
Gil Forcada <gil.gnome@gmail.com>
2017-07-14 00:53:49 +0200

    Make buildout faster

--- package_delta.py
+++ package_delta.py
@@ -12,6 +12,7 @@ buildout -c core.cfg, i.e.:
     virtualenv-2.7 .
     source bin/activate
     pip install -r requirements.txt
+    echo "git-clone-depth = 50" >> checkouts.cfg
     buildout -c core.cfg
 
 Then run this script and update plone_packages.py accordingly to the output.
